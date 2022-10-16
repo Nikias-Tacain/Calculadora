2ca823b75588dd19f0b7621afc563754288c396a
Nikias Tacain <nikiastacain@yahoo.com.ar>
2022-10-15 23:10:05 -0300

    Calculadora v.1

---
+++ app.js
@@ -0,0 +1,8 @@
+let cero = document.getElementById("cero")
+
+cero.addEventListener('click',() =>{
+    let numeroVisor = document.getElementById("numeros")
+    numeroVisor.innerHTML=("0")
+    numeroVisor.appendChild()
+})
+
